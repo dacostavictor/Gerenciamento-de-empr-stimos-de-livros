40e9f3ce5fc4b2033c1a5fed0e80cd24f35fae97
Victor da Costa de Araujo Silva <dacostavictor123@gmail.com>
2026-06-21 11:55:20 -0300

    formulário de cadastro

---
+++ public/script.js
@@ -0,0 +1,4 @@
+const nome = document.getElementById("nome");
+const email = document.getElementById("email");
+const senha = document.getElementById("senha");
+const perfil = document.getElementById("perfil").value;
