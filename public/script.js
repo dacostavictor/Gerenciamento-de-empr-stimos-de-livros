becf9a5e763b9a800bb6d497f56bd0747444debf
Victor da Costa de Araujo Silva <dacostavictor123@gmail.com>
2026-06-21 13:39:47 -0300

    cadastro de usuários

--- public/script.js
+++ public/script.js
@@ -1,4 +1,22 @@
-const nome = document.getElementById("nome");
-const email = document.getElementById("email");
-const senha = document.getElementById("senha");
-const perfil = document.getElementById("perfil").value;
+const formularioCadastro = document.getElementById("formularioCadastro");
+
+function cadastrarUsuario(nome, email, senha, perfil) {
+  fetch("/api/usuarios/cadastrar", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ nome, email, senha, perfil }),
+  }).then(() => {
+    formularioCadastro.reset();
+  });
+}
+
+formularioCadastro.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const nome = document.getElementById("nome").value;
+  const email = document.getElementById("email").value;
+  const senha = document.getElementById("senha").value;
+  const perfil = document.getElementById("perfil").value;
+
+  cadastrarUsuario(nome, email, senha, perfil);
+});
