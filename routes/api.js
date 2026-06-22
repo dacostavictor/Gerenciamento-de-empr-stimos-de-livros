2a4f54c8e2403a0480dd02a907e355a445ba9714
Victor da Costa de Araujo Silva <dacostavictor123@gmail.com>
2026-06-21 22:53:09 -0300

    bug no server corrigido

--- routes/api.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.get("/teste", (req, res) => {
-  res.json({ mensagem: "API funcionando!" });
-});
-
-module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,22 @@
 const express = require("express");
+const app = express();
+const port = 3000;
 const path = require("path");
 
-const app = express();
-
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(express.static("public"));
+const usersRoutes = require("./routes/usuarios");
+app.use("/api/usuarios", usersRoutes);
 
-const apiRoutes = require("./routes/api");
+const booksRoutes = require("./routes/livros");
+app.use("/api/livros", booksRoutes);
 
-app.use("/api", apiRoutes);
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
-app.listen(3000, () => {
-    console.log("Servidor rodando");
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
 });
