12b683123db733d4cfe3e870092d34e0c696e31e
Victor da Costa de Araujo Silva <dacostavictor123@gmail.com>
2026-06-22 17:23:26 -0300

    criando as páginas de leitor e bibliotecario

--- server.js
+++ server.js
@@ -17,6 +17,14 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.get("/bibliotecario", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "bibliotecario.html"));
+});
+
+app.get("/leitor", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "leitor.html"));
+});
+
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
